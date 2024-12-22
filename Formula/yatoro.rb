495b822d379254de21a466d3b5e41632b2baeb79
Herman Berdnikov <applejay1802@gmail.com>
2024-12-22 18:44:29 -0500

    Update yatoro.rb

--- Formula/yatoro.rb
+++ Formula/yatoro.rb
@@ -1,13 +1,28 @@
 class Yatoro < Formula
   desc "VIM-like Apple Music Player in Terminal"
   homepage "https://github.com/jayadamsmorgan/Yatoro"
-  url "https://github.com/jayadamsmorgan/Yatoro/releases/download/v0.2.0/yatoro-0.2.0.zip"
-  sha256 "fd8f00a2072a0a9dc96b70f737ff5285ff54ce983f3748fb7c43cb6de42b2b64"
+  version "0.3.0"
   license "MIT"
-
+  
   depends_on "notcurses"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/jayadamsmorgan/Yatoro/releases/download/v0.3.0/yatoro_macOS-arm64.zip"
+      sha256 "746a614298464bf5c4955d015c7b1cb9b818a2e4a723f92c0c040459640f0780"
+    end
+
+    on_intel do
+      url "https://github.com/jayadamsmorgan/Yatoro/releases/download/v0.3.0/yatoro_macOS-x86_64.zip"
+      sha256 "0569b09956d45980bc3e1173e956315e09350a0c4ff91630a54d0385087d2425"
+    end
+  end
 
   def install
     bin.install "yatoro"
+  end
+
+  test do
+    system "#{bin}/yatoro", "--version"
   end
 end

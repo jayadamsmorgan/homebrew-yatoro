9529d6bd285304ae05e7b5244db9a3cfb294f1e8
Herman Berdnikov <applejay1802@gmail.com>
2026-01-14 18:08:29 -0500

    Update

--- Formula/yatoro.rb
+++ Formula/yatoro.rb
@@ -1,24 +1,17 @@
 class Yatoro < Formula
   desc "VIM-like Apple Music Player in Terminal"
   homepage "https://github.com/jayadamsmorgan/Yatoro"
-  url "https://github.com/jayadamsmorgan/Yatoro/archive/refs/tags/v0.3.3.tar.gz"
-  sha256 "518de974099b11ceb674026e3578f8f09b3fc26e32d83a9eb466a44633360521"
+  url "https://github.com/jayadamsmorgan/Yatoro.git",
+    tag: "v0.3.3",
+    revision: "2df718faa9e15f859f51a6636fe71feafa847d3d"
   version "0.3.3"
   license "MIT"
 
   depends_on "notcurses"
 
   def install
-    # Give Versionator a git repo to query (tarballs don't have .git)
-    system "git", "init"
-    system "git", "config", "user.email", "brew@localhost"
-    system "git", "config", "user.name", "Homebrew"
-    system "git", "add", "."
-    system "git", "commit", "-m", "Homebrew build"
-    system "git", "tag", "v#{version}"
-
     system "swift", "build", "--disable-sandbox", "-Xcc", "-DNCURSES_UNCTRL_H_incl"
-    bin.install ".build/release/yatoro"
+    bin.install ".build/debug/yatoro"
   end
 
   test do
